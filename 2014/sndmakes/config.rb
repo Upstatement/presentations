b14cb41201bf285340cdec91bede315ea26a7f2a
Jared Novack <jared@upstatement.com>
2014-10-18 11:52:00 -0400

    Added SNDMakes Lightning talk

---
+++ 2014/sndmakes/config.rb
@@ -0,0 +1,24 @@
+# This is the config.rb file to support Upbase. Move this to your root folder after you bower install.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+# And this is where compass will watch and compile stuff when it runs:
+# css_dir = "css"
+# sass_dir = "scss"
+css_dir = "css/theme/"
+sass_dir = "css/theme/source"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+relative_assets = true
+
+# This line tells compass to look at the Upbase styles in your bower_components dir
+
+#line_comments = false
+
+line_comments = true
+output_style = :expanded
+# output_style = :compact :compressed :nested :expanded
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
